26644ada601e0cc20e19324b240101466348eb11
shinichi butani <mozquito@me.com>
2014-03-16 21:33:34 +0900

    Checking in changes prior to tagging of version 0.03.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index e03b940..bee82f8 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Reviision history for Perl extension Mojolicious-Plugin-Korpokkur
    
     {{$NEXT}}
    
    +0.03 2014-03-16T12:33:32Z
    +   - generate機能を整理
    +
     0.02 2014-03-13T03:47:49Z
        - 5.14以前にエラーがでるのを対処。

--- lib/Mojolicious/Plugin/Korpokkur/Command/generate.pm
+++ lib/Mojolicious/Plugin/Korpokkur/Command/generate.pm
@@ -1,6 +1,6 @@
 package Mojolicious::Plugin::Korpokkur::Command::generate;
 use Mojo::Base 'Mojolicious::Command::generate';
-our $VERSION = '0.02'; # VERSION
+our $VERSION = '0.03'; # VERSION
 has namespaces => sub { ['Mojolicious::Plugin::Korpokkur::Command::generate'] };
 
 
